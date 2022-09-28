a26f79a8394bb5b07ede850df80f465470dee118
Ricardo Apaza <rapazac22@fpcoruna.afundacion.org>
2022-09-28 14:04:49 +0200

    Bienvenida a actor favorito

---
+++ sprint1apache/www/site3/example2.php
@@ -0,0 +1,12 @@
+<html>
+	<body>
+		<h1>Página de bienvenida</h1>
+		<?php
+			function dar_bienvenida($nombre){
+			echo "¡Bienvenido/a, " . $nombre . "!";
+			}
+		dar_bienvenida("Danny Pudi");
+ 		?>
+	</body>
+</html>
+
