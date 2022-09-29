b95c1d79337cf4b2407b5afd1031f89d24d2ccc3
Ricardo Apaza <rapazac22@fpcoruna.afundacion.org>
2022-09-29 10:58:10 +0200

    conversor2 que acepta yardas subido a repositorio

---
+++ sprint1apache/www/site3/conversor2_v2.php
@@ -0,0 +1,39 @@
+<html>
+	<body>
+		<h1>Conversor de longitudes</h1>
+		<p>Convierte de la unidad especificada a metros</p>
+<p>
+		<?php
+			if(isset($_POST['funidad'])){
+				if($_POST['funidad']=='pulgada'){
+				$v_pulgadas=$_POST['fcantidad'];
+				$v_metros = $v_pulgadas * 0.0254;
+				echo "<p style='color:green'>".$v_pulgadas." pulgada(s) = ".$v_metros." metro(s)</p>";
+				}else if($_POST['funidad'] == 'yarda'){
+				$v_yarda=$_POST['fcantidad'];
+				$v_metros=$v_yarda * 0.9144;
+				echo "<p style='color:green'>".$v_yarda. " yarda(s) = " .$v_metros." metro(s)</p>";
+				}else{
+				echo "<p style='color:red'>Unidad no soportada";
+}
+}
+
+		?>
+</p>
+<p>Realiza una nueva conversión: </p>
+<form action="/conversor2_v2.php" method="post">
+	<label for="cantidad_input">Cantidad:</label><br>
+	<input type="text" id="cantidad_input" name="fcantidad"><br>
+
+	<input type="radio" id="pulgada_input" name="funidad" value="pulgada">
+	<label for="pulgada_input">Pulgada(s)</label><br>
+	<input type="radio" id="yarda_input" name="funidad" value="yarda">
+	<label for="yarda_input">Yarda(s)</label><br>
+	<input type="radio" id="otro_input" name="funidad" value="otro">
+	<label for="otro_input">Otros</label><br>
+	<input type="submit" value="convertir">
+</form>
+
+	</body>
+</html>
+
