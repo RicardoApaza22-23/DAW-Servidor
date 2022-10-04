0e920efa8182a93a7d199971164a281895197f17
RicardoApaza22-23 <113433932+RicardoApaza22-23@users.noreply.github.com>
2022-10-04 14:29:38 +0200

    Add files via upload

---
+++ sprint2phpsql/www/mysite/comment.php
@@ -0,0 +1,21 @@
+<html>
+<?php
+		$db = mysqli_connect('localhost','root','1234','mysitedb') or die('FAIL');
+ 		?>
+<head>
+	<title>Conexión a base de datos</title>
+</head>
+	<body>
+        <?php
+            $juegos_id = $_POST['juegos_id'];
+            $comentario = $_POST['new_comment'];
+            $query = "insert into tComentarios(comentarios,juegos_id,usuario_id) values ('".$comentario."','". $juegos_id ."','null')";
+            mysli_query($db,$query) or die('Error');
+            echo "<p>Nuevo Comentario";
+            echo mysqli_insert_id($db);
+            echo " añadido</p>";
+            echo "<a href='detail.php?juegos_id=" . $juegos_id . "'>Volver</a>";
+            mysqli_close($db);
+        ?>
+	</body>
+</html>
