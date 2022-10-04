326054cefb5d3fc8bed2bf81f4c6b98eb1ebfe68
Ricardo Apaza <rapazac22@fpcoruna.afundacion.org>
2022-10-04 13:42:43 +0200

    main.php

--- sprint2phpsql/www/mysite/main.php
+++ sprint2phpsql/www/mysite/main.php
@@ -8,6 +8,16 @@
 		$db = mysqli_connect('localhost','root','1234','mysitedb') or die('FAIL');
  		?>
 		<h1>Conexión establecida</h1>
+		<?php
+		//lanzar un query
+		$query = "Select * from tJuegos";
+		$resultado = mysqli_query($db,$query) or die("Query error");
+		//recorrer el resultadod
+		while($row = mysqli_fetch_array($resultado)){
+			echo $row[1]. " => " . $row[3]."<br>";
+}
+		mysqli_close($db);
+		?>
 	</body>
 </html>
 
