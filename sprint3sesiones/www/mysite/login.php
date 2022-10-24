b9c939e5898852be152deff202588e12c1752f86
Ricardo Apaza <rapazac22@fpcoruna.afundacion.org>
2022-10-24 13:13:38 +0200

    controlador de login actualizado

--- sprint3sesiones/www/mysite/login.php
+++ sprint3sesiones/www/mysite/login.php
@@ -3,12 +3,17 @@ $db = mysqli_connect('localhost','root','1234','mysitedb') or die('FAIL');
 $email = $_POST['email'];
 $contraseña = $_POST['password'];
 
-$query = "Select email,contraseña from tUsuarios where email='" . $email . "'";
+$query = "Select id,email,contraseña from tUsuarios where email='" . $email . "'";
 $result = mysqli_query($db,$query) or die('Query error');
+/**
+while($row = mysqli_fetch_array($result)){
+	echo $row[1];
+}
+*/
 
-if(mysqli_num_rows($result) > 0){
+if((mysqli_num_rows($result) > 0)){
     $only_row = mysqli_fetch_array($result);
-    if($only_row[4] == $contraseña){
+    if($only_row[2] == $contraseña){
         session_start();
         $_SESSION['user_id']=$only_row[0];
         header("Location: main.php");
@@ -16,9 +21,7 @@ if(mysqli_num_rows($result) > 0){
         echo "<p style='color:red'> Contraseña incorrecta</p>";
     }
 }else{
-    echo "<p style='color:red'>Usuario no encontrado con ese email</p>"
+    echo "<p style='color:red'>Usuario no encontrado con ese email</p>";
 }
-
-
 
 ?>
