c91b9085cd779ff6573762846479924e998ef520
Ricardo Apaza <rapazac22@fpcoruna.afundacion.org>
2022-10-25 13:11:09 +0200

    organización de  tabla añadida para mejor visualizacion

--- Carpeta/sprint3sesiones/www/mysite/main.php
+++ Carpeta/sprint3sesiones/www/mysite/main.php
@@ -1,6 +1,8 @@
 <html>
 <head>
 	<title>Conexión a base de datos</title>
+	<link href="main.css" rel="stylesheet" type="text/css">
+	<link href='https://fonts.googleapis.com/css?family=Rock+Salt' rel='stylesheet' type='text/css'>
 </head>
 	<body>
 
@@ -8,17 +10,32 @@
 		$db = mysqli_connect('localhost','root','1234','mysitedb') or die('FAIL');
  		?>
 		<h1>Conexión establecida</h1>
+		<table border="1" id="tabla">
+			<tr>
+				<th>ID</th>
+				<th>Juego</th>
+				<th>Foto</th>
+				<th>Categoria</th>
+				<th>PEGI</th>
+			</tr>
 		<?php
 		//lanzar un query
 		$query = "Select * from tJuegos";
 		$resultado = mysqli_query($db,$query) or die("Query error");
 		//recorrer el resultadod
 		while($row = mysqli_fetch_array($resultado)){
-			echo "<a href='detail.php?juegos_id=".$row[0]."'>$row[0]</a> ";
-			echo $row[1]. " => " . $row[3]."<br>";
+			echo "<tr>";
+			echo "<td><a href='detail.php?juegos_id=".$row[0]."'>$row[0]</a> </td>";
+			// echo $row[1]. " => " . $row[3]."<br>";
+			echo "<td>".$row[1]."</td>";
+			echo "<td>".$row[2]."</td>";
+			echo "<td>".$row[3]."</td>";
+			echo "<td>".$row[4]."</td>";
+			echo "</tr>";
 }
 		mysqli_close($db);
 		?>
+		</table>
 <br>
 <a href ="/logout.php" > LogOut</a>
 	</body>
