3952da15631336fffbe4a4dd800a6e034cda165b
Ricardo Apaza <rapazac22@fpcoruna.afundacion.org>
2022-10-25 12:54:02 +0200

    archivo comment.php modificado para que se guarde el usuario que hizo el comentario en la BD

--- Carpeta/sprint3sesiones/www/mysite/comment.php
+++ Carpeta/sprint3sesiones/www/mysite/comment.php
@@ -1,42 +1,45 @@
+<?php
+session_start();
+?>
 <html>
 
 <head>
 	<title>Conexión a base de datos</title>
 </head>
-	<body>
-        <?php
 
- 	   $db = mysqli_connect('localhost','root','1234','mysitedb') or die('FAIL');
-	session_start();
-	$userNull = 'NULL';
+<body>
+	<?php
 
-	if(!empty($_SESSION['user_id'])){
-		$userNull = $_SESSION['user_id'];
-}
+	$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('FAIL');
+	
+	//el programa solo acepta registros de la base de datos aunque le pase otros parámetros
 
-            $juegos_id = $_POST['juegos_id'];
-            $comentario = $_POST['new_comment'];
-	    $fecha = date('Y-m-d H:i:s');
-if($comentario != ''){
-$query = "insert into tComentarios(comentarios,usuario_id,juegos_id,fecha) values('".$comentario."','".$userNull ."','".$juegos_id."','$fecha')";
-         mysqli_query($db,$query) or die('Error');
-          	echo "<p>Nuevo Comentario";
-           	echo mysqli_insert_id($db);
-            	echo " añadido</p>";
-            	echo "<a href='detail.php?juegos_id=" . $juegos_id . "'>Volver</a>";
-}else{
+	if (!empty($_SESSION['user_id'])) {
+		$user = $_SESSION['user_id'];
+	}
+	
+	$juegos_id = $_POST['juegos_id'];
+	$comentario = $_POST['new_comment'];
+	$fecha = date('Y-m-d H:i:s');
+	if ($comentario != '') {
+		$query = "insert into tComentarios(comentarios,usuario_id,juegos_id,fecha) values('" . $comentario . "','" . $user . "','".$userNull."','".$fecha."')";
+		mysqli_query($db, $query) or die('Error');
+		echo "<p>Nuevo Comentario";
+		echo mysqli_insert_id($db);
+		echo " añadido</p>";
+		echo "<a href='detail.php?juegos_id=" . $juegos_id . "'>Volver</a>";
+	} else {
 
-echo "<p style='color:red'> Introduzca un comentarios</p>";
-echo "<a href='detail.php?juegos_id=" . $juegos_id . "'>Vovler</a>";
-
-
-}
-
+		echo "<p style='color:red'> Introduzca un comentarios</p>";
+		echo "<a href='detail.php?juegos_id=" . $juegos_id . "'>Vovler</a>";
+	}
 
 
 
-            	mysqli_close($db);
 
-        ?>
-	</body>
+	mysqli_close($db);
+
+	?>
+</body>
+
 </html>
