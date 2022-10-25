68c0e03d59e5adc03ff3df15cc21d8bf2bf07161
Ricardo Apaza <rapazac22@fpcoruna.afundacion.org>
2022-10-25 14:16:05 +0200

    archivo controlador de login actualizada: contraseña hasheada

--- Carpeta/sprint3sesiones/www/mysite/login.php
+++ Carpeta/sprint3sesiones/www/mysite/login.php
@@ -5,17 +5,15 @@ $contraseña = $_POST['password'];
 
 $query = "Select id,email,contraseña from tUsuarios where email='" . $email . "'";
 $result = mysqli_query($db,$query) or die('Query error');
-/**
-while($row = mysqli_fetch_array($result)){
-	echo $row[1];
-}
-*/
+
 
 if((mysqli_num_rows($result) > 0)){
     $only_row = mysqli_fetch_array($result);
-    if($only_row[2] == $contraseña){
+    if(hash_equals(crypt($contraseña,'$2a$07$usesomesillystringforsalt$'),$only_row[2])){
+       
         session_start();
         $_SESSION['user_id']=$only_row[0];
+        $_SESSION['email']=$only_row[1];
         header("Location: main.php");
     }else{
         echo "<p style='color:red'> Contraseña incorrecta</p>";
@@ -23,5 +21,6 @@ if((mysqli_num_rows($result) > 0)){
 }else{
     echo "<p style='color:red'>Usuario no encontrado con ese email</p>";
 }
+
 
 ?>
