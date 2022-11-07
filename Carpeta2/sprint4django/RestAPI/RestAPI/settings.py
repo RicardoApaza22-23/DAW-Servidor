248c0a059b489e55b1506ba91bd5a9a477da1814
Ricardo Apaza <rapazac22@fpcoruna.afundacion.org>
2022-11-07 18:12:35 +0100

    implementada nuestra base de datos en el proyecto REST API

--- Carpeta2/sprint4django/RestAPI/RestAPI/settings.py
+++ Carpeta2/sprint4django/RestAPI/RestAPI/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'RestAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysitedb',
+	'USER': 'root',
+	'PASSWORD': '1234',
+	'HOST': 'localhost',
+	'PORT': '3306'
     }
 }
 
--- Carpeta2/sprint4django/RestAPI/webserviceapp/models.py
+++ Carpeta2/sprint4django/RestAPI/webserviceapp/models.py
@@ -1,3 +1,50 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
+
+class Tcomentarios(models.Model):
+    comentarios = models.CharField(max_length=200, blank=True, null=True)
+    usuario = models.ForeignKey('Tusuarios', models.DO_NOTHING, blank=True, null=True)
+    juegos = models.ForeignKey('Tjuegos', models.DO_NOTHING)
+    fecha = models.DateField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'tComentarios'
+
+
+class Tfavoritos(models.Model):
+    idusuario = models.ForeignKey('Tusuarios', models.DO_NOTHING, db_column='idUsuario')  # Field name made lowercase.
+    idjuego = models.ForeignKey('Tjuegos', models.DO_NOTHING, db_column='idJuego')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'tFavoritos'
+
+
+class Tjuegos(models.Model):
+    nombre = models.CharField(max_length=50, blank=True, null=True)
+    url_image = models.CharField(max_length=200, blank=True, null=True)
+    categoria = models.CharField(max_length=100, blank=True, null=True)
+    pegi = models.CharField(db_column='PEGI', max_length=20, blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'tJuegos'
+
+
+class Tusuarios(models.Model):
+    nombre = models.CharField(max_length=50, blank=True, null=True)
+    apellidos = models.CharField(max_length=100, blank=True, null=True)
+    email = models.CharField(unique=True, max_length=200, blank=True, null=True)
+    contraseña = models.CharField(max_length=200, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'tUsuarios'
