fce9a7d2651ee778153a47f833fdc5d383683447
Ricardo Apaza <rapazac22@fpcoruna.afundacion.org>
2022-11-08 13:40:38 +0100

    añadida a la vista una función para mostrar los datos de un registro en la base de datos a través de un parametro recibido

--- Carpeta2/sprint4django/RestAPI/webserviceapp/views.py
+++ Carpeta2/sprint4django/RestAPI/webserviceapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from .models import Tjuegos
+from .models import Tjuegos, Tcomentarios
 
 
 def pagina_de_prueba(request):
@@ -19,4 +19,22 @@ def devolver_juegos(request):
 		diccionario['PEGI'] = fila_sql.pegi
 		arrayLista.append(diccionario)
 	return JsonResponse(arrayLista,safe=False)
+
+def devolver_juegos_por_id(request,id_solicitado):
+	juego = Tjuegos.objects.get(id=id_solicitado)
+	comentarios = juego.tcomentarios_set.all()
+	lista_comentarios=[]
+	for fila_comentarios_sql in comentarios:
+		diccionario={}
+		diccionario['id'] = fila_comentarios_sql.id
+		diccionario['comentarios'] = fila_comentarios_sql.comentarios
+		lista_comentarios.append(diccionario)
+		resultado = {
+			'id': juego.id,
+			'nombre': juego.nombre,
+			'foto': juego.url_image,
+			'categoria': juego.categoria,
+			'PEGI': juego.pegi
+		}
+	return JsonResponse(resultado, json_dumps_params={'ensure_ascii':False})
 
