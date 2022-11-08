1e0c9e97f40c7f73641369ec6f791075246eedd9
Ricardo Apaza <rapazac22@fpcoruna.afundacion.org>
2022-11-08 12:37:46 +0100

    agregada nueva url de prueba

--- Carpeta2/sprint4django/RestAPI/RestAPI/urls.py
+++ Carpeta2/sprint4django/RestAPI/RestAPI/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from webserviceapp import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test',views.pagina_de_prueba)
 ]
