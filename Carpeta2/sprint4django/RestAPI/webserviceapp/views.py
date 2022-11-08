bf5d930924b1b392d8df6763d87536279f599bf9
Ricardo Apaza <rapazac22@fpcoruna.afundacion.org>
2022-11-08 12:38:21 +0100

    agregada nueva función a vista

--- Carpeta2/sprint4django/RestAPI/webserviceapp/views.py
+++ Carpeta2/sprint4django/RestAPI/webserviceapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def pagina_de_prueba(request):
+	return HttpResponse("<h1>hola</h1>")
+	
+def devolver_juegos(request):
+	#lista=tJuegos.objects.all()
+	
