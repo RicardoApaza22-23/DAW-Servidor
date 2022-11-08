4e100062a521934eaf21646e4d7971615d214ae4
Ricardo Apaza <rapazac22@fpcoruna.afundacion.org>
2022-11-08 13:15:50 +0100

    agregada nueva funcion en vista para mostrar los juegos

--- Carpeta2/sprint4django/RestAPI/webserviceapp/views.py
+++ Carpeta2/sprint4django/RestAPI/webserviceapp/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Tjuegos
+
 
 def pagina_de_prueba(request):
 	return HttpResponse("<h1>hola</h1>")
-	
+	#return lista
+
 def devolver_juegos(request):
-	#lista=tJuegos.objects.all()
-	
+	lista = Tjuegos.objects.all()
+	arrayLista = []
+	for fila_sql in lista:
+		diccionario={}
+		diccionario['id'] = fila_sql.id
+		diccionario['nombre'] = fila_sql.nombre
+		diccionario['foto'] = fila_sql.url_image
+		diccionario['categoria'] = fila_sql.categoria
+		diccionario['PEGI'] = fila_sql.pegi
+		arrayLista.append(diccionario)
+	return JsonResponse(arrayLista,safe=False)
+
