be8973004bc1cc993c38b0edc594e70ed983a4aa
Ricardo Apaza <rapazac22@fpcoruna.afundacion.org>
2022-11-15 13:32:52 +0100

    creamos nuestro modelos

--- CarpetaServidorDjango/mysite/polls/models.py
+++ CarpetaServidorDjango/mysite/polls/models.py
@@ -4,4 +4,7 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question,on_delete=models.CASCADE)
