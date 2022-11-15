748515720eb737bf623510afaa76aa29d5e681b8
Ricardo Apaza <rapazac22@fpcoruna.afundacion.org>
2022-11-15 14:44:59 +0100

    en un primer momento agrego una fila manualmente a la base de datos, pero luego la borro para hacerla mediante migracion

--- CarpetaServidorDjango/mysite/polls/migrations/0001_initial.py
+++ CarpetaServidorDjango/mysite/polls/migrations/0001_initial.py
@@ -25,6 +25,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('question', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='polls.question')),
+                
             ],
         ),
     ]
