3ebfc04e34a04f235667d9e8e37adeafdb0f61cb
Ricardo Apaza <rapazac22@fpcoruna.afundacion.org>
2022-11-16 13:48:21 +0100

    corregido un fallo en el return

--- CarpetaServidorDjango/mysite/polls/models.py
+++ CarpetaServidorDjango/mysite/polls/models.py
@@ -18,4 +18,4 @@ class Choice(models.Model):
     choice_text1 = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     def __str__(self):
-        return self.choice_text
+        return self.choice_text1
