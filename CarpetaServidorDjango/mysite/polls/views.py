aaaff2dab0deab2b471bac9aafc32bfd26b045fb
Ricardo Apaza <rapazac22@fpcoruna.afundacion.org>
2022-11-16 14:14:04 +0100

    corregido fallo en la vista y el url result

--- CarpetaServidorDjango/mysite/polls/views.py
+++ CarpetaServidorDjango/mysite/polls/views.py
@@ -10,7 +10,8 @@ def detail(request, question_id):
     return HttpResponse("You're loogin at question %s." % question_id)
 
 def results(request, question_id):
-    response = HttpResponse("You're loogin at the results of question %s.")
+    response = ("You're loogin at the results of question %s.")
     return HttpResponse(response % question_id)
 def vote(request,question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
