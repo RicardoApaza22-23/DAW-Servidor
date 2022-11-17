3c7af03594bb700507c30ca038fdc8ca31ee140c
Ricardo Apaza <rapazac22@fpcoruna.afundacion.org>
2022-11-17 12:08:34 +0100

    modificado la función vote para guardar los votos en la BD

--- CarpetaServidorDjango/mysite/polls/views.py
+++ CarpetaServidorDjango/mysite/polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse, Http404
-from .models import Question
+from django.http import HttpResponse, Http404,HttpResponseRedirect
+from .models import Question, Choice
 from django.template import loader
+from django.urls import reverse
 
 
 
@@ -26,6 +27,20 @@ def detail(request, question_id):
 def results(request, question_id):
     response = ("You're loogin at the results of question %s.")
     return HttpResponse(response % question_id)
+
 def vote(request,question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html',{
+            'question' : question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
+        
 
