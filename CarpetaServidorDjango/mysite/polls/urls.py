5cd533b6ad014ae59e54635de56945995f15763a
Ricardo Apaza <rapazac22@fpcoruna.afundacion.org>
2022-11-21 13:26:34 +0100

    cambios mínimos(oruebas)

--- CarpetaServidorDjango/mysite/polls/urls.py
+++ CarpetaServidorDjango/mysite/polls/urls.py
@@ -5,7 +5,8 @@ app_name='polls'
 
 urlpatterns = [
     path('index',views.index,name='index'),
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.detail , name='detail'),
     path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
+    
 ]
