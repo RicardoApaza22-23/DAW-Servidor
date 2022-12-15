4d962f3cb8b872116338dea7b5ff644a36111d2c
Ricardo Apaza <rapazac22@fpcoruna.afundacion.org>
2022-12-15 19:38:04 +0100

    formulario modificado: agregado fotos, celdas y fuentes

---
+++ opcional_pdf/www/mysite/formulario.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <title>IMPRIMIR PDF</title>
+
+</head>
+<h3>DATOS DE CERTIFICADO</h3>
+<body>
+    <form action="generar_pdf.php" method="post" id="formulario">
+        <input name="name" type="text" placeholder="nombre" id="nombre"><br>
+        <input name="surname" type="text" placeholder="apellidos" id="apellidos"><br>
+        <br>
+        <input type="submit" value="enviar">
+    </form>
+</body>
+
+</html>
