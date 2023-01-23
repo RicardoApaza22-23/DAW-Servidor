700118e95874bec41478780ccaa5f59d976562a2
Ricardo Apaza <rapazac22@fpcoruna.afundacion.org>
2023-01-23 13:19:00 +0100

    vistas básicas para mostrar los usuarios

--- Proyecto-DAWServidor/ProyectoStoreApi/webserviceapp/views.py
+++ Proyecto-DAWServidor/ProyectoStoreApi/webserviceapp/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse,JsonResponse
+from django.http import HttpResponse, JsonResponse
 from .models import Usuarios
+from django.shortcuts import render, get_object_or_404
 
 
 def mostrarUsuarios(request):
     usuarios = Usuarios.objects.all()
     respuesta_final = []
     for data in usuarios:
-        listaUsuarios={}
+        listaUsuarios = {}
         listaUsuarios['id'] = data.id
         listaUsuarios['nombre'] = data.nombre
+        listaUsuarios['correo'] = data.correo
         listaUsuarios['telefono'] = data.telefono
         listaUsuarios['direccion'] = data.direccion
         listaUsuarios['edad'] = data.edad
@@ -17,4 +19,20 @@ def mostrarUsuarios(request):
 
         respuesta_final.append(listaUsuarios)
     return JsonResponse(respuesta_final, safe=False)
- 
+
+
+def mostrarUsuarioID(request, idUsuario):
+    usuariosID = get_object_or_404(Usuarios, pk=idUsuario)
+    respuesta_final = []
+
+    respuesta = {}
+    respuesta['id'] = usuariosID.id
+    respuesta['nombre'] = usuariosID.nombre
+    respuesta['correo'] = usuariosID.correo
+    respuesta['telefono'] = usuariosID.telefono
+    respuesta['direccion'] = usuariosID.direccion
+    respuesta['edad'] = usuariosID.edad
+    respuesta['rol'] = usuariosID.rol
+    respuesta_final.append(respuesta)
+    return JsonResponse(respuesta_final,safe=False)
+    
