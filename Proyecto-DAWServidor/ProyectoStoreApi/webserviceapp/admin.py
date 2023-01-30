a8c9b4b76f46ea2dd61520796cc2177a5d9a34e7
Ricardo Apaza <rapazac22@fpcoruna.afundacion.org>
2023-01-30 12:34:11 +0100

    modificado archivi admin.py para algunos modelos

--- Proyecto-DAWServidor/ProyectoStoreApi/webserviceapp/admin.py
+++ Proyecto-DAWServidor/ProyectoStoreApi/webserviceapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Usuarios,Producto, Comentario
+admin.site.register(Usuarios)
+admin.site.register(Producto)
+admin.site.register(Comentario)
