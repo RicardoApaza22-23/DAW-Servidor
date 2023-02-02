665149e8923d5bad84483a1b23e5ebc680e58c5b
Ricardo Apaza <rapazac22@fpcoruna.afundacion.org>
2023-02-02 11:40:34 +0100

    nuevas vistas añadidas

--- Proyecto-DAWServidor/ProyectoStoreApi/webserviceapp/views.py
+++ Proyecto-DAWServidor/ProyectoStoreApi/webserviceapp/views.py
@@ -1,7 +1,7 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from .models import Usuarios, Producto, Comentario
+from .models import Usuarios, Producto, Comentario, Favoritos, Compra
 from django.shortcuts import render, get_object_or_404
 import json
 from datetime import datetime
@@ -51,29 +51,39 @@ def registrar(request):
     json_peticion = json.loads(request.body)
     nuevo_usuario = Usuarios()
     nuevo_usuario.nombre = json_peticion['nombre']
-    nuevo_usuario.correo = "pruebacorreo"#json_peticion['correo']
-    nuevo_usuario.telefono = "123123123" #json_peticion['direccion'
-    nuevo_usuario.direccion = "direccionprueba"
-    nuevo_usuario.edad = "19" #json_peticion['edad']
-    nuevo_usuario.rol = "0" #json_peticion['rol']
+    nuevo_usuario.correo = json_peticion['correo'] #"pruebacorreo"#json_peticion['correo']
+    nuevo_usuario.telefono = json_peticion['telefono'] #"123123123" #json_peticion['direccion'
+    nuevo_usuario.direccion = json_peticion['direccion']#"direccionprueba"
+    nuevo_usuario.edad = json_peticion['edad'] #"19" #json_peticion['edad']
+    nuevo_usuario.rol = json_peticion['rol']#"0" #json_peticion['rol']
     #cmnuevo_usuario. = "passwordprueba" #json_peticion['password']
     nuevo_usuario.save()
     return JsonResponse({"status": "ok"})
     #return JsonResponse(json_peticion['nombre'])         
-            
-            
+   
+@csrf_exempt            
+def delete_user(request,id_usuario):
+    usuario = Usuarios.objects.get(id = id_usuario)
+    usuario.delete()
+    
+    return JsonResponse({"status": "ok"})
+
+
+ 
 def mostrarProductos(request):
     producto = Producto.objects.all()
-    
     mostrar_productos = []
+    
     for data in producto:
         respuesta={}
         respuesta['nombre'] = data.nombre
-        respuesta['estado'] = data.estado    
-       
+        respuesta['estado'] = data.estado     
         #usuario = Usuarios.objects.filter(producto__vendedor=1)
-        respuesta['vendedor'] = 1#1#corregir      
-        
+        usuarios = Usuarios.objects.get(id = data.vendedor.id)
+        diccionario = {}
+        diccionario['id'] = usuarios.id
+        diccionario['nombre'] = usuarios.nombre    
+        respuesta['vendedor'] = diccionario              
         respuesta['estacion'] = data.estacion
         respuesta['precio'] = data.precio
         respuesta['color'] = data.color
@@ -87,9 +97,14 @@ def mostrarProductos(request):
 
 def mostrarProductoID(request,id_producto):
     productos = Producto.objects.get(id = id_producto)
-    #usuario = Producto.objects.filter(usuarios__id=1)
-    usuario = Usuarios.objects.filter(id = productos.vendedor)
+
+    usuario = Usuarios.objects.get(id = productos.vendedor.id)
     lista_producto = []
+    diccionario = {}
+    diccionario['id'] = usuario.id
+    diccionario['nombre'] = usuario.nombre
+    lista_producto.append(diccionario)
+    
     resultado = {
         'id' : productos.id,
         'nombre' : productos.nombre,
@@ -100,49 +115,36 @@ def mostrarProductoID(request,id_producto):
         'talla' : productos.talla,
         'categoría' : productos.categoria,
         'fecha de subida' : productos.fecha_de_subida,
-        'vendedor' : usuario.id
+        'vendedor' : lista_producto
         
     }
     
     return JsonResponse(resultado,safe=False)
-
-    """
-    for productoID in productos:
-        producto_objeto = {}
-        producto_objeto["id"] = productoID.id
-        producto_objeto["nombre"] = productoID.nombre
-        producto_objeto["estado"] = productoID.estado
-        usuario = Usuarios.objects.filter(id = productoID.vendedor)
-        producto_objeto["estacion"] = productoID.estacion
-        producto_objeto["precio"] = productoID.precio
-        producto_objeto["color"] = productoID.color
-        producto_objeto["talla"] = productoID.talla
-        producto_objeto["categoría"] = productoID.categoria
-        producto_objeto["fecha_de_subida"] = productoID.fecha_de_subida
-    for data in usuario:
-        vendedor = data.id #vendedor['id'] #1#corregir
-        
-    """
     
-
-    
-    
+@csrf_exempt   
 def crearProducto(request):
-    usuario = get_object_or_404(Usuarios,pk=1)
+    usuario = get_object_or_404(Usuarios,id = 1)
     
     nuevo_producto = Producto()
-    nuevo_producto.nombre = "jordan 2"
-    nuevo_producto.estado = "nuevo"
-    nuevo_producto.vendedor = 1 #vendedor
-    nuevo_producto.estacion = "primavera"
+    nuevo_producto.nombre = "jordan 3"
+    nuevo_producto.estado = "segunda mano"
+    nuevo_producto.vendedor = usuario #vendedor
+    nuevo_producto.estacion = "invierno"
     nuevo_producto.precio = 222
-    nuevo_producto.color = "blanco y morado"
-    nuevo_producto.talla = "22"
+    nuevo_producto.color = "verde y morado"
+    nuevo_producto.talla = "40"
     nuevo_producto.categoria = "sneaker"
     nuevo_producto.fecha_de_subida = datetime.now()
     nuevo_producto.save()
     return JsonResponse({"status": "ok"})
-""" 
+
+@csrf_exempt  
+def delete_producto(request, producto_id):
+    producto = Producto.objects.get(id = producto_id)
+    producto.delete()
+    return JsonResponse({"status": "ok"})
+
+@csrf_exempt  
 def crear_comentarios_al_producto(request,id_producto):
     productoID = get_object_or_404(Producto, pk = id_producto)
     #esto será innecesario si se usa el id del usuario guardado en la session
@@ -155,7 +157,8 @@ def crear_comentarios_al_producto(request,id_producto):
     nuevo_comentario.fecha = datetime.now()
     nuevo_comentario.save()
     return JsonResponse({"status" : "ok"})
-  """
+
+
 def mostrar_comentarios_por_id(request,id_prod):
     producto = Producto.objects.get(id = id_prod)
     comentarios = Comentario.objects.filter(id_producto = id_prod)
@@ -174,4 +177,97 @@ def mostrar_comentarios_por_id(request,id_prod):
     
     return JsonResponse(resultado,safe=False)
         
-       
+def mostrar_favoritos(request):
+    favoritos = Favoritos.objects.all()
+    
+    lista_favoritos = []
+    for data in favoritos:
+        diccionario = {}
+        diccionario['id_producto'] = data.id_producto.id
+        diccionario['id_usuarios'] = data.id_usuarios.id
+        diccionario['fecha'] = datetime.now()
+        
+        usuario = Usuarios.objects.get(id = data.id_usuarios.id)
+        diccionario['nombre_usuario'] = usuario.nombre
+        producto = Producto.objects.get(id = data.id_producto.id)
+        diccionario['nombre_producto'] = producto.nombre
+        lista_favoritos.append(diccionario)
+        
+    return JsonResponse(lista_favoritos,safe=False)
+
+def mostrar_favoritoID(request, favorito_id):
+    favorito = Favoritos.objects.get(id=favorito_id)
+    producto = Producto.objects.filter(id = favorito.id_producto.id)
+    usuario = Usuarios.objects.filter(id = favorito.id_usuarios.id)
+    lista_favorito = []
+    lista_favorito2 = []
+    for data in usuario:
+        diccionario = {}
+        diccionario['id'] = data.id
+        diccionario['nombre_usuario'] = data.nombre
+        lista_favorito.append(diccionario)
+    for data in producto:
+        diccionario2={}
+        diccionario2['id'] = data.id
+        diccionario2['nombre'] = data.nombre
+        lista_favorito2.append(diccionario2)
+    resultado = {
+        'id' : favorito.id,
+        'id_usuario' : favorito.id_usuarios.id,
+        'id_producto' : favorito.id_producto.id,
+        'fecha' : favorito.fecha,
+        'usuario' : lista_favorito,
+        'producto' : lista_favorito2
+        
+    }
+    #lista_favorito.append(resultado)
+    
+    return JsonResponse(resultado,safe=False)
+
+def añadir_favorito(request,producto_id):
+    producto = get_object_or_404(Producto,pk = producto_id)
+    #usuario harcodeado
+    usuarioHC = get_object_or_404(Usuarios,pk = 1)
+    nuevo_favorito = Favoritos()
+    nuevo_favorito.id_usuarios = usuarioHC
+    nuevo_favorito.id_producto = producto
+    nuevo_favorito.fecha = datetime.now()
+    nuevo_favorito.save()
+    return JsonResponse({"status":"ok"})
+    
+    #error: eliminar todas los registros dependiedno del producto_id
+    #posible solucin = agregar una condición más al filter y ponerle que reciba el id_producto
+    #y además un usuario en concreto, para que elimine ciertos campos y no todos con el mismo producto_id
+def delete_favorito(request,producto_id):
+    favorito = Favoritos.objects.filter(id_producto = producto_id)
+    favorito.delete()
+    return JsonResponse({"status" : "ok"})
+
+def mostrar_compras(request):
+    compras = Compra.objects.all()
+    #usuario = Usuarios.objects.all()
+    lista_compras = []
+    for data in compras:
+        diccionario = {}
+        diccionario['id'] = data.id 
+        diccionario['fecha'] = data.fecha
+        lista_compras.append(diccionario)
+    
+        
+
+    return JsonResponse({"status" : "ok"})
+
+
+@csrf_exempt 
+def crear_compra(request):
+    nueva_compra = Compra()
+    #datos harcodeados
+    usuario = get_object_or_404(Usuarios,pk=2)
+    producto = get_object_or_404(Producto,pk=1)
+    
+    nueva_compra.id_comprador = usuario
+    nueva_compra.id_producto = producto
+    nueva_compra.fecha = datetime.now()
+    nueva_compra.save()
+    
+    return JsonResponse({"status" : "ok"})
